2bae87a91d6790998bd082e587f1a5e08b70bbeb
Matthew Bystedt <matthew.bystedt@gov.bc.ca>
2024-12-30 09:45:17 -0800

    feat: improve error handling

--- src/cron/compress.ts
+++ src/cron/compress.ts
@@ -1,7 +1,6 @@
-import * as fs from 'fs';
+import * as fs from 'fs/promises';
 import * as path from 'path';
 import { exec } from 'child_process';
-
 import { DB_FILE_STATUS } from '../constants';
 import { DatabaseService } from '../services/database.service';
 
@@ -22,50 +21,57 @@ export async function compress(db: DatabaseService) {
   );
   for (const row of result.rows) {
     const compressedFilePath = `${row.path}.tgz`;
-    // Clear compressed file if it already exists
-    if (fs.existsSync(compressedFilePath)) {
-      // Remove uncompressed file
-      fs.rmSync(compressedFilePath);
-    }
-    if (!fs.existsSync(row.path)) {
-      // Delete missing log
-      db.deleteLog(row.id);
-      continue;
-    }
 
-    await new Promise<void>((resolve, reject) => {
-      const cmd = `tar -zcvf ${row.path}.tgz -C ${path.dirname(
-        row.path,
-      )} ${path.basename(row.path)}`;
-      console.log(`compress: ${cmd}`);
-      exec(cmd, (error, stdout, stderr) => {
-        if (error) {
-          // node couldn't run the command
-          reject(error);
-          return;
-        }
-        // Using process.stdout.write to prevent double new lines.
-        if (stdout) {
-          process.stdout.write(`compress: [stdout] ${stdout}`);
-        }
-        if (stderr) {
-          process.stdout.write(`compress: [stderr] ${stderr}`);
-        }
+    try {
+      // Clear compressed file if it already exists
+      if (await fs.stat(compressedFilePath).catch(() => false)) {
+        await fs.rm(compressedFilePath);
+      }
 
-        db.run(
-          `
-          UPDATE logs
-          SET status = ?,
-          path = ?
-          WHERE id = ?
-          `,
-          [DB_FILE_STATUS.Compressed, compressedFilePath, row.id],
-        );
-        // Remove uncompressed file
-        fs.rmSync(row.path);
+      // Check if the log file exists
+      if (!(await fs.stat(row.path).catch(() => false))) {
+        await db.deleteLog(row.id);
+        continue;
+      }
 
-        resolve();
+      await new Promise<void>((resolve, reject) => {
+        const cmd = `tar -zcvf ${row.path}.tgz -C ${path.dirname(
+          row.path,
+        )} ${path.basename(row.path)}`;
+        console.log(`compress: ${cmd}`);
+        exec(cmd, async (error, stdout, stderr) => {
+          if (error) {
+            // node couldn't run the command
+            reject(error);
+            return;
+          }
+          // Using process.stdout.write to prevent double new lines.
+          if (stdout) {
+            process.stdout.write(`compress: [stdout] ${stdout}`);
+          }
+          if (stderr) {
+            process.stdout.write(`compress: [stderr] ${stderr}`);
+          }
+
+          try {
+            await db.run(
+              `
+              UPDATE logs
+              SET status = ?,
+              path = ?
+              WHERE id = ?
+              `,
+              [DB_FILE_STATUS.Compressed, compressedFilePath, row.id],
+            );
+            await fs.rm(row.path);
+            resolve();
+          } catch (dbError) {
+            reject(dbError);
+          }
+        });
       });
-    });
+    } catch (error) {
+      console.error(`Error compressing file ${row.path}:`, error);
+    }
   }
 }
